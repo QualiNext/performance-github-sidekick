7c9cb464354ad238991852efd25e49c14531c402
Evgeny Khaliper <evgeny.k@quali.com>
2024-09-17 15:01:23 +0300

    init

---
+++ terraform/s3/main.tf
@@ -0,0 +1,40 @@
+# s3_module/main.tf
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = var.bucket_name
+}
+
+resource "aws_s3_object" "file_upload" {
+  bucket  = aws_s3_bucket.my_bucket.id
+  key     = var.file_name
+  content = var.file_content
+}
+
+# s3_module/variables.tf
+
+variable "bucket_name" {
+  description = "Name of the S3 bucket"
+  type        = string
+}
+
+variable "file_name" {
+  description = "Name of the file to be uploaded to S3"
+  type        = string
+}
+
+variable "file_content" {
+  description = "Content of the file to be uploaded"
+  type        = string
+}
+
+# s3_module/outputs.tf
+
+output "bucket_name" {
+  description = "Name of the created S3 bucket"
+  value       = aws_s3_bucket.my_bucket.id
+}
+
+output "file_url" {
+  description = "URL of the uploaded file"
+  value       = "https://${aws_s3_bucket.my_bucket.bucket_regional_domain_name}/${aws_s3_object.file_upload.key}"
+}
